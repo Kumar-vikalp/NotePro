5931110f09b23b2565fc7bc99b5db8eef9a946e2
vikalp <kumarvikalp48@gmail.com>
2025-07-27 02:08:55 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-cd backend
-
+# Install from root-level requirements.txt
 python3 -m pip install --upgrade pip setuptools wheel
 python3 -m pip install -r requirements.txt
+
+# Move to backend for Django management
+cd backend || exit
 
 python3 manage.py collectstatic --noinput
 
